e090a53d0544c75565e59bfd354d7c014fd7caf6
unknown <MAITUMELO RADIPODIm>
2022-06-03 20:36:19 +0200

    new

--- objects_and_classes/main.py
+++ objects_and_classes/main.py
@@ -1,14 +1,31 @@
+from tokenize import Name
+
+
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    
+    
+   
+    def __init__(self,name,age,tracks,score ):
+            self.name = name
+            self.age=age
+            self.tracks = tracks
+            self.score=score
+    pass
+    
+    
+    def change_name( self, new_name):
+        print("changed name" + self.name)
+        
+    r1 = Name("Neo")
+    
+    r1.change_name()
 
 
-
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+''' Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
 Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
-Bob.get_score()
+Bob.get_score() '''
